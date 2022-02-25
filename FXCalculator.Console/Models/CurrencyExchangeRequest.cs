40904bd75bd9c665c472f20ebf721a7d9017d7c8
yves-d <40821202+yves-d@users.noreply.github.com>
2022-02-25 23:00:49 +1100

    started adding console app

---
+++ FXCalculator.Console/Models/CurrencyExchangeRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FXCalculator.Console.Models
+{
+    public class CurrencyExchangeRequest
+    {
+        public string Base { get; init; }
+        public string Term { get; init; }
+        public decimal Amount { get; init; }
+
+    }
+}
---
+++ FXCalculator.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Welcome to the ANZx FXCalculator!" + Environment.NewLine);
+Console.WriteLine("---------------------------------" + Environment.NewLine);
+Console.WriteLine(Environment.NewLine);
+Console.WriteLine("Type 'exit' at any time, to end the program" + Environment.NewLine);
+Console.WriteLine(Environment.NewLine);
+Console.WriteLine("To convert a currency, enter the base currency, followed by the amount, the word 'in', and finally the term currency you wish to conver to. For example..." + Environment.NewLine);
+Console.WriteLine("AUD 100.00 in USD" + Environment.NewLine);
+
+bool exit = false;
+var command = Console.ReadLine();
+while (!exit)
+{
+    if (command.ToLower() == "exit")
+    {
+        exit = true;
+        continue;
+    }
+    try
+    {
+        CommandReader commandReader = new CommandReader(command, currentArena, currentRobot);
+        commandReader.ExecuteCommand();
+
+        command = Console.ReadLine();
+        currentArena = commandReader.CommandReaderArena;
+        currentRobot = commandReader.CommandReaderRobot;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        command = Console.ReadLine();
+    }
+}
